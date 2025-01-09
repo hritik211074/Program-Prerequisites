bc49485a2c400aca3179fead15f2b193b1d9b5a4
HRITIK KUMAR MAURYA <166793651+hritik211074@users.noreply.github.com>
2025-01-09 11:49:21 +0530

    Changing formulas

--- Convert_Kilometers_to_Miles.java
+++ Convert_Kilometers_to_Miles.java
@@ -4,7 +4,7 @@ public class Convert_Kilometers_to_Miles{
     public static void main(String args[]){
        Scanner sc =new Scanner(System.in);
 	   double km_distance=sc.nextDouble();
-	   double mile_distance=km_distance*0.621371;
+	   double mile_distance=km_distance*1.6;
 	   System.out.println("Conversion from km to miles is->>"+(mile_distance));
 	}
  }
